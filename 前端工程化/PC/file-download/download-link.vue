d0f57b3574c228076d355ea9b849d01f1355517b
wind-stone <wind-stone@qq.com>
2018-06-28 11:03:03 +0800

    PC 文件下载相关

---
+++ 前端工程化/PC/file-download/download-link.vue
@@ -0,0 +1,49 @@
+<template>
+  <a v-if="isSupportDownload" class="download-link" :href="url" :download="name">
+    <slot v-if="$slots.default"></slot>
+    <span v-else v-text="text"></span>
+  </a>
+  <a v-else class="download-link" href="#" @click.stop.prevent="download(url)">
+    <slot v-if="$slots.default"></slot>
+    <span v-else v-text="text"></span>
+  </a>
+</template>
+
+<script>
+export const isSupportDownload = 'download' in document.createElement('a')
+
+export default {
+  name: 'download-link',
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      default: '下载文件'
+    },
+    text: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  data () {
+    return {
+      isSupportDownload
+    }
+  },
+  methods: {
+    download (url) {
+      window.open(url, '_blank', 'fullscreen=no,width=400,height=300')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.download-link {
+  text-decoration: none;
+  color: #409eff;
+}
+</style>
