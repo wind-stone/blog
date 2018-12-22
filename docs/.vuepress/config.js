20cb6cb0fb02f80ecbcd25971cf0e3bff803ec33
wind-stone <wind-stone@qq.com>
2018-12-22 13:38:01 +0800

    feat: flex 兼容性问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -254,6 +254,7 @@ module.exports = {
           title: '移动端 - CSS 兼容性',
           collapsable: true,
           children: [
+            'compatibility/css/flex',
             'compatibility/css/ie',
             'compatibility/css/android-small-fontsize'
           ]
