1821d7849df2c6d8dd5d66fb12877c25a82806e1
wind-stone <wind-stone@qq.com>
2019-12-11 08:59:04 +0800

    feat(vue): 修改 CascadeVisibility 组件以支持动画

--- docs/.vuepress/components/cascade-visibility/CascadeVisibility.js
+++ docs/.vuepress/components/cascade-visibility/CascadeVisibility.js
@@ -20,12 +20,15 @@ export default {
         selfVisible(newVal) {
             this.$emit(EVENT_TOGGLE, newVal);
         },
-        visible: {
-            handler(newVal) {
-                newVal ? this.show() : this.hide();
-            },
+    },
+    mounted() {
+        // 放在此处而不是跟 selfVisible 一起放在 watch 选项里，是为了实现动画效果
+        // 若是放在 watch 选项里，就不会产生动画，因为 selfVisible 一创建组件是就是 true，没有一个从 false --> true 的过程
+        this.$watch('visible', (newVal) => {
+            newVal ? this.show() : this.hide();
+        }, {
             immediate: true
-        }
+        });
     },
     methods: {
         show() {
