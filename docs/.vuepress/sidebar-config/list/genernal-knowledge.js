240fbeeec6e4041c2b7980774e88c3a8277c64f2
wind-stone <wind-stone@qq.com>
2020-03-12 22:34:08 +0800

    feat(nginx): nginx 实践

--- docs/.vuepress/sidebar-config/list/genernal-knowledge.js
+++ docs/.vuepress/sidebar-config/list/genernal-knowledge.js
@@ -1,55 +1,56 @@
 module.exports = {
-  // General Knowledge
-  '/general-knowledge/': [
-    {
-      title: 'CLI',
-      collapsable: true,
-      children: [
-        'cli/',
-        'cli/command'
-      ]
-    },
-    {
-      title: 'Git',
-      collapsable: true,
-      children: [
-        'git/git-command',
-        'git/git-commit-guidelines',
-        'git/gitlab',
-        'git/'
-      ]
-    },
-    {
-      title: 'Nginx',
-      collapsable: true,
-      children: [
-        'nginx/',
-        'nginx/nginx-conf',
-        'nginx/ngx_http_proxy_module',
-        'nginx/ngx_http_upstream_module'
-      ]
-    },
-    {
-      title: 'Network',
-      collapsable: true,
-      children: [
-        'network/dns'
-      ]
-    },
-    {
-      title: 'Server',
-      collapsable: true,
-      children: [
-        'server/'
-      ]
-    },
-    {
-      title: '其他',
-      collapsable: true,
-      children: [
-        'others/glob',
-        'others/google-cloud'
-      ]
-    }
-  ]
+    // General Knowledge
+    '/general-knowledge/': [
+        {
+            title: 'CLI',
+            collapsable: true,
+            children: [
+                'cli/',
+                'cli/command'
+            ]
+        },
+        {
+            title: 'Git',
+            collapsable: true,
+            children: [
+                'git/git-command',
+                'git/git-commit-guidelines',
+                'git/gitlab',
+                'git/'
+            ]
+        },
+        {
+            title: 'Nginx',
+            collapsable: true,
+            children: [
+                'nginx/',
+                'nginx/practices',
+                'nginx/nginx-conf',
+                'nginx/ngx_http_proxy_module',
+                'nginx/ngx_http_upstream_module'
+            ]
+        },
+        {
+            title: 'Network',
+            collapsable: true,
+            children: [
+                'network/dns'
+            ]
+        },
+        {
+            title: 'Server',
+            collapsable: true,
+            children: [
+                'server/'
+            ]
+        },
+        {
+            title: '其他',
+            collapsable: true,
+            children: [
+                'others/glob',
+                'others/google-cloud'
+            ]
+        }
+    ]
 };
