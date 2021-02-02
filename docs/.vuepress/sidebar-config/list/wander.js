4a8fda22124d4c335347d86371f7ec1d66202d5f
wind-stone <wind-stone@qq.com>
2021-02-02 09:59:43 +0800

    feat: 投资理财

--- docs/.vuepress/sidebar-config/list/wander.js
+++ docs/.vuepress/sidebar-config/list/wander.js
@@ -1,35 +1,42 @@
 module.exports = {
-  // 随记
-  '/wander/': [
-    {
-      title: 'House 相关',
-      collapsable: false,
-      children: [
-        'house/',
-        'house/tianya-post',
-        'house/beijing/',
-        'house/zhonghailichunhushu/',
-        'house/wanxiangyuefu/',
-        'house/xishanjinxiufu/',
-        'house/comparison'
-      ]
-    },
-    {
-      title: '户外',
-      collapsable: false,
-      children: [
-        'outdoor/',
-      ]
-    },
-    {
-      title: '其他',
-      collapsable: false,
-      children: [
-        // 'mac-shortcut-keys',
-        // 'word-pronunciation',
-        'others/theories',
-        'others/ssr/'
-      ]
-    }
-  ]
+    // 随记
+    '/wander/': [
+        {
+            title: 'House 相关',
+            collapsable: false,
+            children: [
+                'house/',
+                'house/tianya-post',
+                'house/beijing/',
+                'house/zhonghailichunhushu/',
+                'house/wanxiangyuefu/',
+                'house/xishanjinxiufu/',
+                'house/comparison'
+            ]
+        },
+        {
+            title: '投资理财',
+            collapsable: false,
+            children: [
+                'investment/',
+            ]
+        },
+        {
+            title: '户外',
+            collapsable: false,
+            children: [
+                'outdoor/',
+            ]
+        },
+        {
+            title: '其他',
+            collapsable: false,
+            children: [
+                // 'mac-shortcut-keys',
+                // 'word-pronunciation',
+                'others/theories',
+                'others/ssr/'
+            ]
+        }
+    ]
 };
